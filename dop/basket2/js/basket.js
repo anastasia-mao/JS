4d8e8c0e199f553e60885b23ae90eea145c57fa3
anastasia-mao <markelova.ao@yandex.ru>
2017-01-09 23:37:15 +0300

    basket2-catalogTemplate

---
+++ dop/basket2/js/basket.js
@@ -0,0 +1,115 @@
+// старый вариант, сейчас нужен для корзины
+var testjson = '[ \
+		{"key":1, "price":100, "itemName":"Картоха", "itemImg":"img/potato.png"},\
+		{"key":2,"price":200,"itemName":"Маркоха","itemImg":"img/Carrot.jpg"},\
+		{"key":3,"price":300,"itemName":"Манго","itemImg":"img/mango.jpg"}\
+]'
+
+// для шаблонизатора (line 48)
+var data = { "test": [
+		{"key":1, "price":100, "itemName":"Картоха", "itemImg":"img/potato.png"},
+		{"key":2,"price":200,"itemName":"Маркоха","itemImg":"img/Carrot.jpg"},
+		{"key":3,"price":300,"itemName":"Манго","itemImg":"img/mango.jpg"}
+        ]}
+
+function addElement(parent, tag, attr, text){
+	var par = document.getElementById(parent),
+	elem = document.createElement(tag),
+	text = text || "";
+	for (a in attr){
+		elem.setAttribute(attr[a][0],attr[a][1]);
+	}
+	elem.innerHTML = text;
+	par.appendChild(elem);
+}
+
+
+function getJsonCount(json){
+	var items = JSON.parse(json);
+	return items.length;
+}
+
+function getJsonObjectKeys(json, object, keys){
+	var items = JSON.parse(json),
+	itemsArr = [];
+	for (k in keys){
+		itemsArr.push(items[object][keys[k]])
+	}
+	return itemsArr;
+}
+
+function findKey(json, key){
+	for (i=0; i<getJsonCount(testjson); i++){
+		if (getJsonObjectKeys(testjson, i, ["key"])==key) return i;
+	}
+}
+
+// отрисовка каталога шаблонизатором
+function catalog(){
+    template = "<h2 id=\"catalogHead\" class=\"catalogHead\">Каталог</h2>\
+        {{#test}}<div id=\"catalogItemId{{key}}\" class=\"catalogItem\">\
+        <img class=\"catalogImg\" src=\"{{itemImg}}\">\
+        <h3 class=\"catalogHeader\">{{itemName}}</h3>\
+        <h5 class=\"catalogPrice\">{{price}}</h5>\
+        <button id=\"add{{key}}\" onclick=\"basket.add('{{key}}',1); basket.clear(); basket.render();\">Добавить</button></div>{{/test}}";
+        
+    result = Mustache.render(template, data);
+    var dest = document.getElementById('catalog');
+	dest.innerHTML = result;
+}
+
+catalog();
+
+var basket = {
+	items: {},
+	sum: 0,
+	add:function(key, count){
+		if (key in basket.items){
+			basket['items'][key]+= count;
+		}
+		else {
+			basket['items'][key]=count;
+		}
+	},
+	del:function(itemIndex){
+			delete basket.items[itemIndex+1]; //itemIndex+1 т.к. itemIndex - индекс в basket.items, а удаляется по ключу
+			document.getElementById('basketItemId'+itemIndex).remove();
+	},
+	recalc:function(){
+		this.sum = 0;
+		for ( i in basket.items ){
+			this.sum+=basket.items[i]*getJsonObjectKeys(testjson, findKey(testjson,i), ['price'])[0];
+		}
+		return this.sum;
+	},
+	render:function(){
+		this.recalc;
+		for ( i in this.items ){
+			var item = findKey(testjson,i),
+			itemKeys = getJsonObjectKeys(testjson, item, ["itemName", "price", "itemImg"]),
+			itemName = itemKeys[0],
+			price = itemKeys[1],
+			itemImg = itemKeys[2];
+			console.log('img:'+itemImg);
+			addElement('basket', 'div', [[ 'id','basketItemId'+i ], ['class', 'basketItem']]);
+			addElement('basketItemId'+i, 'img', [ ['class', 'basketImg'], ['src',itemImg] ]);
+			addElement('basketItemId'+i, 'h3', [ ['class', 'basketHeader'] ], itemName);
+			addElement('basketItemId'+i, 'input', [ ['type', 'text'],['class', 'basketCount'],['value', this.items[i+1]] ] );
+			addElement('basketItemId'+i, 'h5', [ ['class', 'basketPrice'] ], price);
+			addElement('basketItemId'+i, 'button', [ ['id', 'del'+i], ['onclick','basket.del('+i+');basket.clear(); basket.render();'] ], 'Удалить');
+		}
+		addElement('basket', 'span', [[ 'id','sumtext'], ['class', 'sumtext']], 'Итого: ');
+		addElement('basket', 'span', [[ 'id','sum'], ['class', 'sum']], this.recalc());
+	},
+	clear:function(){
+		document.getElementById('basket').remove();
+		var catalog = document.getElementById('catalog'),
+		div = document.createElement('div');
+		div.id = 'basket';
+		div.className = 'basket';
+		body.insertBefore(div,document.getElementById('catalog'));
+		addElement('basket', 'h2', [[ 'id','basketHead'], ['class', 'basketHead']], 'Корзина');
+	}
+	
+}
+
